c73cf51ae34befe53fdef9706c553c01ae66806c
DviadFer <diegoviadfer@gmail.com>
2024-03-02 13:59:16 +0100

    Include stylesheet with hook on functions.php
    Using wp_enqueue_scripts and then wp_enqueue_style, we can link dynamically css stylesheets.
    For more info check https://developer.wordpress.org/reference/functions/wp_enqueue_style/

--- functions.php
+++ functions.php
@@ -5,3 +5,11 @@
  * 
  * @package Custom theme
  */
+
+ function custom_theme_scripts() {
+    wp_enqueue_style( 'main-styles', get_template_directory_uri() . '/style.css' );
+
+    // More simple version: wp_enqueue_style( 'style-name', get_stylesheet_uri() );
+ }
+
+ add_action( 'wp_enqueue_scripts', 'custom_theme_scripts' );
