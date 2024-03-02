fe364f21162d619e3167acf92bd58506ba3e6ce1
DviadFer <diegoviadfer@gmail.com>
2024-03-02 12:50:30 +0100

    Add wp_head and wp_footer functions
    These two hooks enable enqueuing of scripts and style sheets within wp framework without the need of hardcoding imports or includes.
    Also, they import some default scripts that enable standard wordpress functionalities.

--- footer.php
+++ footer.php
@@ -7,5 +7,7 @@
 ?>
 
 <footer>This is the footer</footer>
+
+<?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -12,6 +12,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wordpress custom theme</title>
+    <?php wp_head(); ?>
 </head>
 <body>
     <header>This is the header</header>
