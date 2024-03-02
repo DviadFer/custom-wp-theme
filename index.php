f98b8319f6dda369a8565e99c58674621b0bba60
DviadFer <diegoviadfer@gmail.com>
2024-03-02 11:51:52 +0100

    Add core files and gitignore

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Main template file. 
+ * Only index.php and style.css are needed to run a custom theme (core files).
+ * style.css must have a header: https://developer.wordpress.org/themes/basics/main-stylesheet-style-css/
+ * 
+ * @package Custom theme
+ * 
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main page</title>
+</head>
+<body>
+    <main>Hello world!</main>
+</body>
+</html>
